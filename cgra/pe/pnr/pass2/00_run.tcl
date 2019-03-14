04813af2b591c541df6483d3f24fbd7057a68c2a
Akash Levy <akashlevy@gmail.com>
2019-03-14 06:53:49 -0700

    PNR pass 2

---
+++ cgra/pe/pnr/pass2/00_run.tcl
@@ -0,0 +1,17 @@
+set_host_options -max_cores 2
+
+set user_name $::env(USER)
+set design_name test_pe
+
+set rtl_dir ../rtl
+set syn_dir ../syn
+set par_dir .
+
+
+source ./01_design_setup.tcl
+source ./02_design_planning.tcl
+source ./03_placement.tcl
+source ./04_cts.tcl
+source ./05_route.tcl
+source ./06_dfm.tcl
+source ./07_streamout.tcl
---
+++ cgra/pe/pnr/pass2/04_cts.tcl
@@ -0,0 +1,20 @@
+set_preferred_routing_direction -layers {M1 M3 M5 M7 M9} -direction horizontal
+set_preferred_routing_direction -layers {M2 M4 M6 M8 MRDL} -direction vertical
+
+report_clock
+report_clock -skew
+report_clock_tree -summary
+report_constraint -all
+
+set_fix_hold_options -effort high
+set_fix_hold [all_clocks]
+
+check_physical_design -stage pre_clock_opt
+
+clock_opt -fix_hold_all_clocks
+report_clock_tree
+report_timing
+
+derive_pg_connection -power_net $MW_POWER_NET -ground_net $MW_GROUND_NET -power_pin $MW_POWER_PORT -ground_pin $MW_GROUND_PORT
+
+save_mw_cel -as ${design_name}_cts.CEL
