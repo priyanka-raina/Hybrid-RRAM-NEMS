cc84683ba43aae02a1633b95ea4fb08670257058
Akash Levy <akashlevy@gmail.com>
2019-04-09 02:43:30 -0700

    CRS plots, presentation updates, untangled xbar

--- measurements/dual_rram.py
+++ measurements/dual_rram.py
@@ -1,47 +1,86 @@
+from datetime import datetime
 import glob
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
+ms = []
 for f in glob.glob('dual_rram/*.csv'):
     lines = open(f).readlines()
+    m = {}
     for i, line in enumerate(lines):
         entries = [entry.strip() for entry in line.split(',')]
         if entries[0] == 'SetupTitle':
-            setup = entries[1]
+            m['setup'] = entries[1]
         elif entries[0] == 'TestParameter':
             if entries[1] == 'Measurement.Primary.Start':
-                startv = entries[2]
+                m['startv'] = entries[2]
             elif entries[1] == 'Measurement.Primary.Stop':
-                stopv = entries[2]
+                m['stopv'] = entries[2]
             elif entries[1] == 'Measurement.Primary.Step':
-                stepv = entries[2]
+                m['stepv'] = entries[2]
             elif entries[1] == 'Measurement.Primary.Compliance':
-                cc = entries[2]
+                m['cc'] = entries[2]
             elif entries[1] == 'Measurement.Primary.Locus':
-                sweepdbl = entries[2] == 'Double'
+                m['sweepdbl'] = (entries[2] == 'Double')
         elif entries[0] == 'MetaData':
             if entries[1] == 'TestRecord.RecordTime':
-                rectime = entries[2] #datetime.parse(entries[2])
+                m['rectime'] = datetime.strptime(entries[2], '%m/%d/%Y %H:%M:%S')
             elif entries[1] == 'TestRecord.Flag':
-                flag = (entries[2] == 'true')
+                m['flag'] = (entries[2] == 'true')
             elif entries[1] == 'TestRecord.Remarks':
-                remarks = entries[2]
+                m['remarks'] = entries[2]
             elif entries[1] == 'TestRecord.TestTarget':
-                devices = entries[2:]
+                m['devices'] = entries[2:]
         elif entries[0] == 'DataName':
             startidx = i
             break
-    data = pd.read_csv(f, skiprows=startidx).drop(columns=['DataName'])
-    print f
-    print 'Setup:', setup
-    print 'Start V:', startv
-    print 'Stop V:', stopv
-    print 'Step V:', stepv
-    print 'Compliance current:', cc
-    print 'Sweep forward and backward:', sweepdbl
-    print 'Record time:', rectime
-    print 'Flag:', flag
-    print 'Remarks:', remarks
+    m['data'] = pd.read_csv(f, skiprows=startidx, skipinitialspace=True).drop(columns=['DataName'])
+    ms.append(m)
+
+
+for m in sorted(ms, key=lambda k: k['rectime'])[89:]:
+    print 'Setup:', m['setup']
+    print 'Devices:', m['devices']
+    print 'Start V:', m['startv']
+    print 'Stop V:', m['stopv']
+    print 'Step V:', m['stepv']
+    print 'Compliance current:', m['cc']
+    print 'Sweep forward and backward:', m['sweepdbl']
+    print 'Record time:', m['rectime']
+    print 'Flag:', m['flag']
+    print 'Remarks:', m['remarks']
     print
-    print data
-    exit()
+    print m['data']
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6.5, 8))
+    if m['setup'] != 'Resistance':
+        titlestr = '%s, devices %s, cc = %s A\nflag = %s, remarks: %s'
+        ax1.set_title(titlestr % (m['setup'], m['devices'], m['cc'], m['flag'], m['remarks']))
+    else:
+        resistance = np.mean(m['data']['Resistance'][1:])
+        titlestr = '%s, devices %s, cc = %s A\nflag = %s, remarks: %s, R = %s $\Omega$'
+        ax1.set_title(titlestr % (m['setup'], m['devices'], m['cc'], m['flag'], m['remarks'], resistance))
+    plt.xlabel('$V_{row}$ (V)')
+    curves = []
+    try:
+        icurve, = ax1.plot(m['data']['Vtop'], m['data']['Itop'])
+        ax1.legend([icurve], ['$I_{row}$'])
+    except KeyError:
+        icurve, = ax1.plot(m['data']['Vrow'], m['data']['absIrow'])
+        ax1.plot(m['data']['Vrow'][20], m['data']['absIrow'][20], color='green', marker='o')
+        ax1.plot(m['data']['Vrow'][-21:-20], m['data']['absIrow'][-21:-20], color='red', marker='o')
+        ax3 = ax1.twinx()
+        vgcurve, = ax3.plot(m['data']['Vrow'], m['data']['Vgate'], color='C1')
+        ax3.plot(m['data']['Vrow'][20], m['data']['Vgate'][20], color='green', marker='o')
+        ax3.plot(m['data']['Vrow'][-21:-20], m['data']['Vgate'][-21:-20], color='red', marker='o')
+        r1curve, = ax2.plot(m['data']['Vrow'][1:-1], m['data']['R1'][1:-1])
+        ax2.plot(m['data']['Vrow'][20], m['data']['R1'][20], color='green', marker='o')
+        ax2.plot(m['data']['Vrow'][-21:-20], m['data']['R1'][-21:-20], color='red', marker='o')
+        r2curve, = ax2.plot(m['data']['Vrow'][1:-1], m['data']['R2'][1:-1])
+        ax2.plot(m['data']['Vrow'][20], m['data']['R2'][20], color='green', marker='o')
+        ax2.plot(m['data']['Vrow'][-21:-20], m['data']['R2'][-21:-20], color='red', marker='o')
+        ax1.legend([icurve, vgcurve], ['$I_{row}$', '$V_{gate}$'])
+        ax2.legend([r1curve, r2curve], ['$R_1$', '$R_2$'])
+    except KeyError:
+        pass
+    plt.show()
