de19d2eb10d70676fc19b55351c34bdd51f3d8f7
Akash Levy <akashlevy@gmail.com>
2019-05-10 12:25:18 -0700

    Generation was done incorrectly before so remove

--- cgra/saed32/pe/pnr/pass1/run.sh
+++
@@ -1,1 +0,0 @@
-icc_shell -gui -f 00_run.tcl 2>&1 | tee output.log
--- cgra/saed32/pe/pnr/pass3/000_setup.tcl
+++
@@ -1,9 +0,0 @@
-set_host_options -max_cores 2
-
-set user_name $::env(USER)
-set design_name test_pe
-
-set rtl_dir ../rtl
-set syn_dir ../syn
-set par_dir .
-
