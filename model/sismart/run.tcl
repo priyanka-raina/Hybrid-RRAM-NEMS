9327aa5799a6ec9303e83ced7260609a6e7e8e57
Akash Levy <akashlevy@gmail.com>
2019-06-24 03:18:15 -0700

    Add SiliconSmart stuff and reorganize

---
+++ model/sismart/run.tcl
@@ -0,0 +1,24 @@
+set cells {DUALRRAM, NEM4T}
+set charpoint devicetest
+create $charpoint 
+set_log_file $charpoint/sis.log
+
+exec cp configure.tcl ${charpoint}/config/configure.tcl
+set_location  $charpoint 
+
+#FR with just netlists
+import -fast -extension .cdl -netlist_dir netlists/ $cells
+
+#FR + Liberty input
+#import -fast -extension .cdl -netlist_dir netlists/ -liberty import.lib -recognize $cells
+
+exit
+
+configure -fast -timing -power -ccs $cells
+characterize $cells
+
+model -verilog -output my_model $cells
+model -timing -power -output nldm $cells
+model -timing -power -ccs -output ccs $cells
+
+log_info "IAMDONE"
